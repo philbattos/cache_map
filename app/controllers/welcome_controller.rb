079a8776192d24a5149cb28bf1050e9dabd1e6bd
Phil Battos <philbattos@gmail.com>
2013-05-28 23:24:50 -0600

    created rails app with three basic pages - home, search, map; added google map api to map page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+class WelcomeController < ApplicationController
+
+  def home
+
+  end
+
+  def search
+
+  end
+
+  def map
+    # render :template => "map"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CacheMap::Application.config.secret_token = '8dde0886c05a17810fdc13738b85ec3c9e537338b4c1975fcf2747c1d95f70f528627a234dc1dd123874107bb195b731404614a28e09ec8fc9a845bcb81b8d78'
